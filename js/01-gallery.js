37bf53522e73ca4734e6d2d522ab565f98e11009
Yulika Zagorovsky <ju.zagorovsky@gmail.com>
2022-09-14 00:14:36 +0200

    adds galleries

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('div');
+galleryEl.addEventListener('click', onClickShowModal);
+
+function onClickShowModal(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const { src, alt } = event.target;
+  const instance = basicLightbox.create(`<img src="${src}" alt="${alt}"/>`);
+  instance.show();
+
+  if (basicLightbox.visible()) {
+    galleryEl.addEventListener('keydown', onKeyDownCloseModal);
+    function onKeyDownCloseModal(event) {
+      event.preventDefault();
+      if (event.key === 'Escape') {
+        instance.close();
+        galleryEl.removeEventListener('keydown', onKeyDownCloseModal);
+      }
+    }
+  }
+}
+
+function makeGallery(arr) {
+  const galleryItemsMarkup = arr
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a></div>`;
+    })
+    .join('');
+  return galleryEl.insertAdjacentHTML('afterbegin', galleryItemsMarkup);
+}
+
+makeGallery(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+// // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('ul.gallery');
+
+galleryEl.addEventListener('click', onClickShowModal);
+
+function onClickShowModal(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const gallery = new SimpleLightbox('ul.gallery a');
+  galleryEl.removeEventListener('click', onClickShowModal);
+  gallery.on('show.simplelightbox', function () {
+    gallery.options.captionDelay = 250;
+  });
+}
+
+function makeGallery(arr) {
+  const galleryItemsMarkup = arr
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}" title="${description}" /></a>`;
+    })
+    .join('');
+  return galleryEl.insertAdjacentHTML('afterbegin', galleryItemsMarkup);
+}
+
+makeGallery(galleryItems);
