71008f0fc8b349d054b48a677cf481835840a956
yulikafsd <ju.zagorovsky@gmail.com>
2026-05-09 19:38:06 +0200

    add task 4 solution

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,25 @@
+'use strict';
 
+const refs = {
+    formElem: document.querySelector('.login-form'),
+};
+
+refs.formElem.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const formData = {
+        userEmail: refs.formElem.elements.email.value.trim(),
+        userPassword: refs.formElem.elements.password.value.trim(),
+    };
+
+    const formDataValues = Object.values(formData);
+
+    if (formDataValues.includes('')) {
+        alert('All form fields must be filled in');
+        return;
+    }
+
+    e.submitter.blur();
+    console.log(formData);
+    refs.formElem.reset();
+});
